5f0de3e9777de54d55d802993e3b64efaafd056c
AlexKazz <akazenoff@gmail.com>
2022-10-18 14:05:01 -0700

    add form functionality

--- src/components/GenreButtons.js
+++ src/components/GenreButtons.js
@@ -5,7 +5,10 @@ function GenreButtons() {
   return (
     <div className='flex justify-center h-10 my-5'>
       {allGenres.map((genre) => (
-        <button className='bg-cyan-600 rounded-md px-2 mx-2 flex-grow-1'>
+        <button
+          key={genre.id}
+          className='bg-cyan-600 rounded-md px-2 mx-2 flex-grow-1'
+        >
           {genre.genre}
         </button>
       ))}
--- src/components/LyricInput.js
+++ src/components/LyricInput.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function LyricInput() {
-  return <div className='bg-red-400 w-1/3'>LyricInput</div>;
+  const [lyrics, setLyrics] = useState('');
+
+  const addLyric = (e) => {
+    e.preventDefault();
+    if (e.target[0].value) {
+      const newArr = [...lyrics, { id: uuidv4(), lyric: e.target[0].value }];
+      setLyrics(newArr);
+      e.target[0].value = '';
+      //   console.log(newArr);
+    }
+  };
+
+  const deleteLyric = (lyricId) => {
+    let newArr = lyrics.filter((lyric) => lyricId !== lyric.id);
+    setLyrics(newArr);
+  };
+
+  return (
+    <div className='bg-red-400 w-1/3 flex-col justify-center px-2'>
+      <form onSubmit={addLyric}>
+        <input placeholder='Start typing!' type='text' />
+        <input type='submit' value='' />
+      </form>
+      <div>
+        {lyrics
+          ? lyrics.map((lyric) => (
+              <p key={uuidv4()}>
+                {lyric.lyric}{' '}
+                <button onClick={() => deleteLyric(lyric.id)}>❌</button>
+              </p>
+            ))
+          : null}{' '}
+      </div>
+    </div>
+  );
 }
 
 export default LyricInput;
