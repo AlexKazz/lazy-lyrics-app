00a0bea8794228117f7371afdc510232dce9d311
AlexKazz <akazenoff@gmail.com>
2022-10-20 22:36:42 -0700

    clean up layout, add submit button for phone use

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,9 +4,7 @@ function Title() {
   return (
     <div className='font-fell flex-col text-center text-custom-white text-4xl pt-3'>
       Lazy Lyrics
-      <div className='text-custome-white text-3xl pt-4 mt-6'>
-        Select A Genre
-      </div>
+      <div className='text-custome-white text-3xl pt-4 mt-6'>Enter a word</div>
     </div>
   );
 }
