5ea55a7ec4e18e1d5ad90550903cb97905d1a6c5
AlexKazz <akazenoff@gmail.com>
2022-11-04 13:07:56 -0700

    consolidate axios calls

--- src/components/LyricInput.js
+++ src/components/LyricInput.js
@@ -12,52 +12,6 @@ function fixer(str) {
 }
 
 function LyricInput({ setLyrics, lyrics }) {
-  async function getTrackIdBtn(e) {
-    const randomPage = Math.ceil(Math.random() * 10);
-    const randomIndex = Math.floor(Math.random() * 10);
-    if (e.target[0].value)
-      try {
-        const res = await axios.get(
-          `http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${e}&page=${randomPage}&apikey=${process.env.REACT_APP_MM_KEY}`
-        );
-        const trackId =
-          res.data.message.body.track_list[randomIndex].track.track_id;
-
-        const artist =
-          res.data.message.body.track_list[randomIndex].track.artist_name;
-
-        const song =
-          res.data.message.body.track_list[randomIndex].track.track_name;
-
-        getTrackLyrics();
-        async function getTrackLyrics() {
-          try {
-            const newRes = await axios.get(
-              `http://api.musixmatch.com/ws/1.1/track.snippet.get?track_id=${trackId}&apikey=${process.env.REACT_APP_MM_KEY}`
-            );
-            const snippet = newRes.data.message.body.snippet.snippet_body;
-            const fixedSnippet = fixer(snippet);
-            const newArr = [
-              ...lyrics,
-              {
-                id: uuidv4(),
-                lyric: fixedSnippet,
-                song: song,
-                artist: artist,
-                prompt: e.target[0].value,
-              },
-            ];
-
-            setLyrics(newArr);
-          } catch (err) {
-            console.log(err);
-          }
-        }
-      } catch (err) {
-        console.log(err);
-      }
-  }
-
   async function getTrackId(e) {
     e.preventDefault();
     const randomPage = Math.ceil(Math.random() * 10);
@@ -115,7 +69,7 @@ function LyricInput({ setLyrics, lyrics }) {
         />
         <input type='submit' value='' />
         <button
-          onClick={() => getTrackIdBtn}
+          onClick={() => getTrackId}
           className='bg-white shadow-lg bg-opacity-95 border border-slate-400 px-2 py-1 my-5 rounded-sm hover:bg-slate-100'
         >
           Submit
