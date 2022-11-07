6eb0bdf481e9bc6ae2e320f64d13dbefed3557d0
AlexKazz <akazenoff@gmail.com>
2022-11-07 15:35:45 -0800

    functioning with env key

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const FULL_KEY = require('dotenv').config();
+const MY_KEY = FULL_KEY.parsed.MY_KEY;
 const express = require('express');
 const app = express();
 const port = 4000;
@@ -7,9 +9,9 @@ let randomPage = Math.ceil(Math.random() * 10);
 
 app.get('/track', async (req, res, next) => {
   const userInput = req.query.userInput;
-  console.log('userInput*****', userInput);
+
   const response = await axios.get(
-    `http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${userInput}&page=${randomPage}&apikey=ef27af1947002457bfe9d02f36c223ac`
+    `http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${userInput}&page=${randomPage}&apikey=${MY_KEY}`
   );
 
   res.send(response.data);
@@ -18,12 +20,11 @@ app.get('/track', async (req, res, next) => {
 app.get('/lyrics', async (req, res, next) => {
   const trackId = req.query.trackId;
   const response = await axios.get(
-    `http://api.musixmatch.com/ws/1.1/track.snippet.get?track_id=${trackId}&apikey=ef27af1947002457bfe9d02f36c223ac`
+    `http://api.musixmatch.com/ws/1.1/track.snippet.get?track_id=${trackId}&apikey=${MY_KEY}`
   );
   res.send(response.data);
-  console.log('server trackId 👉', trackId);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Lazy Lyrics app listening on port ${port}`);
 });
