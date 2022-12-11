4819183e5112dd3f2268e4542b1cf8f3bc41b0a4
AlexKazz <akazenoff@gmail.com>
2022-12-11 11:32:43 -0800

    add toastify for copy notifcation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { clear } from './lyricsSlice';
 import { copyLyrics } from '../utils/lyricFunctions';
+import { ToastContainer } from 'react-toastify';
 
 function Footer() {
   const lyrics = useSelector((state) => state.lyrics);
   const dispatch = useDispatch();
 
   return (
-    <div className='font-fell mt-6 pt-10 text-slate-600'>
+    <div className='font-fell mt-6 pt-10 text-slate-600 flex'>
       <button
         onClick={() => copyLyrics(lyrics)}
         className='shadow-lg bg-opacity-95 bg-white border border-slate-600 px-5 py-1 mr-5 rounded-sm hover:bg-slate-100'
       >
         Copy
       </button>
-
+      <ToastContainer position='bottom-left' theme='colored' autoClose={1000} />
       <button
         onClick={() => dispatch(clear())}
         className='shadow-lg bg-opacity-95 bg-white border border-slate-600 px-5 py-1 rounded-sm hover:bg-slate-100'
--- src/utils/lyricFunctions.js
+++ src/utils/lyricFunctions.js
@@ -1,8 +1,18 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const notify = (lyrics) => {
+  lyrics.length
+    ? toast.success('Lyrics Copied!')
+    : toast.warn('Nothing to Copy!');
+};
+
 export const copyLyrics = (lyrics) => {
   const array = lyrics.map((lyric) => lyric.snippet + '\n');
   const string = array.toString();
   const finalString = string.replaceAll(',', '');
   navigator.clipboard.writeText(finalString);
+  notify(lyrics);
 };
 
 export const fixer = (str) => {
